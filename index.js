99f28d64880dcde1a32d10d645ae6982299202f6
hassanaref <hassan.aref159@gmail.com>
2021-12-30 12:01:11 +0200

    0

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
 const express = require('express');
 const app = express();
 
+const url = 'https://en.wikipedia.org/wiki/Landing_page'
 
-
+axios(url)
+    .then(response => {
+        const html = response.data
+        const $ = cheerio.load(html)
+        const articles = []
+        $('.mw-headline', html).each(function(){
+            const subheader = $(this).text()
+            articles.push({
+                subheader
+            })
+        })
+        console.log(articles)
+    })
 app.listen(3000, () => {
     console.log('server started');
 })
